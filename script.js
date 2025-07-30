0ab97a6350ac75aada411a75878e9dcc4f924c36
Refilwe15 <mrefilwe880@gmail.com>
2025-07-30 11:11:47 +0200

    responsive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
  
-
+//This function checks if the radio button is selected ,if its not selected the alert will be triggered
 document.getElementById("btn").addEventListener("click", function() {
     const selected = document.querySelector('input[name="rate"]:checked');
     if (!selected) {
