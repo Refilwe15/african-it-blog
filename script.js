ddecb2cd0de1f7419a4cc147f6626f2afb53daf5
Refilwe15 <mrefilwe880@gmail.com>
2025-07-30 15:49:36 +0200

    javascript updated

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
  
-//This function checks if the radio button is selected ,if its not selected the alert will be triggered
-document.getElementById("btn").addEventListener("click", function() {
-    const selected = document.querySelector('input[name="rate"]:checked');
-    if (!selected) {
-      alert("Select at least one option.");
 
-    } 
-  });
+document.getElementById("btn").addEventListener("click", function() {
+  const selected = document.querySelector('input[name="rate"]:checked');
+  if (!selected) {
+    alert("Select at least one option.");
+  } else {
+    
+    alert("You selected: " + selected.value);
+  }
+});
